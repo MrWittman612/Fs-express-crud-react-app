8da174d336bc950947cb42ae9090fdbf924c851a
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-07-03 22:29:38 -0500

    add code for heroku push

--- app.js
+++ app.js
@@ -17,4 +17,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 module.exports = app;
