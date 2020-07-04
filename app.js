fac74297fa11cfaa6a388b966051e70629b0e303
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-07-03 22:24:57 -0500

    concurrently script working toke out jest

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var usersRouter = require('./server/api/routes/users');
-var indexRouter = require('./server/api/routes/index');
+// var indexRouter = require('./server/api/routes/index');
 
 var app = express();
 
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
