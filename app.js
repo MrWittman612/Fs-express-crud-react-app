087a58c3cbcc76445cbe2325f41c6e622ddd143f
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-07-03 20:41:11 -0500

    fix import location

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./server/api/routes/users');
+var indexRouter = require('./server/api/routes/index');
 
 var app = express();
 
