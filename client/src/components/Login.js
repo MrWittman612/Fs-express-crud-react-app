169dd118d1bacaa85b094307e027ed8a25b0903b
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-07-11 00:53:54 -0500

    init branch commit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,6 +17,7 @@ export default function Login() {
 
   const loginUser = async (event) => {
     event.preventDefault();
+
     console.log('log formData::', formData);
     const config = {
       headers: {
